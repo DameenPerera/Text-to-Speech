1a4da47b7fba0ca7fd1a8545c748a7320d3f6375
DameenPerera <dameencc95@gmail.com>
2024-07-07 19:19:46 +0530

    Update Program.cs

--- Echo Text/Program.cs
+++ Echo Text/Program.cs
@@ -16,7 +16,7 @@ namespace Echo_Text
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormMainF());
         }
     }
 }
